c80bc2587fbd53b10eb0005024dfbb5a56e8360c
Case Scally <caseScally@gmail.com>
2021-04-20 15:48:23 -0500

    formatted cart total

--- src/CartTotal.js
+++ src/CartTotal.js
@@ -1,10 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import NumberFormat from "react-number-format";
 
-function CartTotal({ getTotalPrice }) {
+function CartTotal({ getTotalPrice, getCount }) {
   return (
     <Container>
-      <Subtotal>Subtotal (2 items): ${getTotalPrice()}</Subtotal>
+      <Subtotal>
+        Subtotal ({getCount()} items):
+        <NumberFormat
+          value={getTotalPrice()}
+          displayType={"text"}
+          thousandSeparator={true}
+          prefix={"$"}
+        />
+      </Subtotal>
       <CheckoutButton>Proceed to checkout</CheckoutButton>
     </Container>
   );
